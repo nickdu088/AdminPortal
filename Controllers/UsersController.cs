76c34a6f1a303823966d9331646a921a555fc267
nickdu088 <26584779+nickdu088@users.noreply.github.com>
2022-11-14 18:57:32 +1100

    Added user controller to get all users.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using test.Data;
+using test.Models;
+
+namespace test.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        ApplicationDbContext context;
+        public UsersController(ApplicationDbContext applicationDbContext)
+        {
+            context = applicationDbContext;
+        }
+        [HttpGet]
+        public IEnumerable<ApplicationUser> Get()
+        {
+            return context.Users;
+        }
+    }
+}
